db66471b4d8bece5d10afb840ccf3a393fa7bc93
flo.deckert <flo.deckert@outlook.com>
2023-11-14 19:54:20 +0100

    fix hick-up when walking and crossing tiles

--- src/entity/walk.asm
+++ src/entity/walk.asm
@@ -50,7 +50,7 @@ right:
 @not_border:
 	ldy #Entity::levelx
 	lda (r3),y
-	and #$08
+	and #$0f
 	sta bSaveXt								; save tile index
 
 	jsr Entities::check_collision_right		; R0 is on tile on the left of the current position
@@ -138,7 +138,7 @@ left:
 @not_border:
 	ldy #Entity::levelx
 	lda (r3),y
-	and #$08
+	and #$0f
 	sta bSaveXt								; save tile index
 
 	jsr Entities::check_collision_left
@@ -211,68 +211,34 @@ check_still_ground:
 	; did the gravity point crossed a tile
 	ldy #Entity::levelx
 	lda (r3),y
-	and #$08
-	cmp bSaveXt
-	beq @keep_walking			; didn't cross
+	and #$0f
+	cmp #$07					; TODO : must be a more optimzed way
+	beq @seven					; TODO : to detect moving from 8 to 7 or 7 to 8
+	cmp #$08
+	beq @eight
+	bra @keep_walking
+@seven:
+	lda bSaveXt
+	cmp #08
+	bne @keep_walking
+	bra @crossed
+@eight:
+	lda bSaveXt
+	cmp #07
+	bne @keep_walking
+@crossed:
 	jsr get_collision_feet		; yes it did
-@check_controler1:				; read the next tile
+@check_controler:				; read the next tile
 	lda (r0),y					; r0 got updated with the new tile index
-	beq @fall					; empty tile, so should be falling
+	beq @change_controler		; empty tile, so should be falling
 	tax							; pass the new tile content
 	lda tiles_attributes,x
 	bit #TILE_ATTR::SOLID_GROUND
-	bne @keep_walking			; we are still on a tile with controler WALK
-	bra @normal_check			; check
-	
-@fall:							; test if we have to enter falling mode
-	tya
-	clc
-	adc  #LEVEL_TILES_WIDTH
-	tay							; check below the gravity point
-	lda (r0),y					; check the tile below
-	tax
-	lda tiles_attributes,x
-	bit #TILE_ATTR::SLOPE
-	beq @change_controler		; change to the gravity controler
-@move_on_slope:					; force the entity position on the top of the below slope
-	jsr Entities::position_y_inc
-	bra @keep_walking
-
-@normal_check:
-	; if the gravity point changed tile, recheck
-	; compare the previous  gravity point tile with the new one
-	clc
-	lda r0
-	adc bTilesHeight
-	sta bSaveX
-	lda r0+1
-	adc #00
-	sta bSaveX + 1 				; save the feet tile index
-	jsr get_collision_feet
-	clc
-	lda r0
-	adc bTilesHeight
-	sta r0
-	lda r0+1
-	adc #00
-	sta r0 + 1 					; get the new feet tile index
-
-	lda r0 + 1
-	cmp bSaveX + 1
-	bne @check_controler		; new feet index <> old feet index
-	lda r0
-	cmp bSaveX
-	bne @check_controler		; new feet index <> old feet index
+	beq @change_controler			; we are still on a tile with controler WALK
 @keep_walking:
 	lda #00						; continue with the WALK controler
 	rts
-
-@check_controler:				; read the next tile
-	lda (r0)					; r0 got updated with the new tile index
-	tax							; pass the new tile content
-	lda tiles_attributes,x
-	bit #TILE_ATTR::SOLID_GROUND
-	bne @keep_walking			; we are still on a tile with controler WALK
 @change_controler:
 	jmp Entities::go_class_controler; check the object based set_controler
+	
 .endscope
